10eb5a17f048048851cab693bce6ef3de8836489
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2019-03-13 22:59:19 +0700

    init

---
+++ simpleweb/index.js
@@ -0,0 +1,23 @@
+/*
+const express = require('express');
+const app = express();
+
+app.get('./', (req, res) => {
+    res.send('Hi there');
+});
+
+app.listen(8080, ()=> {
+    console.log('Listening on port 8080');
+});
+*/
+const express = require('express');
+ 
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send('How are you doing?');
+});
+ 
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
+});
