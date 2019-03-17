30d6bc6113c8ad9bc30a3fdb183945600bf5d437
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2019-03-17 12:31:54 +0700

    add complex section

---
+++ complex/server/index.js
@@ -0,0 +1,41 @@
+const keys = require('./keys');
+
+
+//Express App Setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// Postgres client setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
+pgClient.on('error', () => console.log('Lost pg connection'));
+
+pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.log(err));
+
+
+//Redis client setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+
+//
