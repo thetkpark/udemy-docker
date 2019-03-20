edb7f04085c80d443dd24a0dcc0cf7eda3d8c3b7
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2019-03-20 21:55:49 +0700

    update react-route-dom to 5.0

--- complex-me/client/src/Fib.js
+++ complex-me/client/src/Fib.js
@@ -21,7 +21,7 @@ class Fib extends Component {
     async fetchIndexes(){
         const seenIndexes = await axios.get('/api/values/all');
         this.setState({
-            seenIndexes: seenIndexes.data;
+            seenIndexes: seenIndexes.data
         });
     }
 
